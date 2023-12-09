02002c55492ad2752eed092f8cadcc8e66d9ea56
DESKTOP-R0GJI59\sitis <sitisihan@myyahoo.com>
2023-12-09 19:17:58 +0100

    Did a small change!

--- js/main.js
+++ js/main.js
@@ -481,6 +481,12 @@ allLinks.forEach(function (link) {
         if (href !== "#" && href.startsWith("#")) {
             const sectionEl = document.querySelector(href);
             sectionEl.scrollIntoView({ behavior: "smooth" });
+            let image = document.getElementById('mobile-nav');
+            if(window.getComputedStyle(image).display !== "none"){
+                image.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAL0lEQVR4nO3BQREAMBADofVvOpXQ/w1QAEDtiHZEAADwtSPaEQEAwNeOaEcEANQDM6arjUtOdLsAAAAASUVORK5CYII=";
+            }else{
+                image.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAACXBIWXMAAAsTAAALEwEAmpwYAAABBUlEQVR4nO3ZTQqDMBCGYU/RSq9Yj1uwPc1bRBcirWicv4T5Vm6UeVCTGdJ1mUwm00yAJ9AHqKOfaim9eWDO6IlhRkw1TBlKHnADXssDPsBDpdL9Gu6rGt7FNXhikEJ4YpBGeGDQQlhi0EZYYMwQmhhzhAbGDSGJcUdIYMIgrmDCIUowYRFnMOERRzDVIPYw1SH+zBDj5tp9ULvyZup6E+tsPiezEUA0TXxa/PixI4zNp7K3OlWD4cASGx7DiX0iLIaCzS4chgs7dhgMAm2HOwbB3skNg0IDaI5BsYs1w2DQiqONsZwn0MJ4DEVIYzwnO6QwEcZTJDDNHL01cxiayWQyXcR8AfkY5euVO0c7AAAAAElFTkSuQmCC";
+            }
         }
 
         // Close mobile naviagtion
